fed7fbc04b3dfca1acec29b6458f4cabba1af956
Alex Forrester <alexforrester96@gmail.com>
2019-10-03 11:42:10 +0100

    added create new student

--- hogwarts_controller.rb
+++ hogwarts_controller.rb
@@ -10,3 +10,14 @@ get "/students" do
   @students = Student.all()
   erb(:index)
 end
+
+#NEW
+get "/students/new" do
+  erb( :new )
+end
+
+post "/students" do
+  @student = Student.new(params)
+  @student.save()
+  erb(:create)
+end
